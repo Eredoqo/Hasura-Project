574d40395a64d5b9f0f81eb4286eaccc365a2ade
Eredoqo <eredoqorri96@gmail.com>
2022-06-10 09:46:47 +0000

    git push Validation

--- hasura-ecomm/migrations/default/1654852829530_rooms/up.sql
+++ hasura-ecomm/migrations/default/1654852829530_rooms/up.sql
@@ -10,3 +10,14 @@ CREATE TABLE "public"."rooms" (
 CONSTRAINT "rooms_pkey" PRIMARY KEY ("id")
 
  );
+ALTER TABLE ONLY "public"."rooms"
+ADD CONSTRAINT "rooms_user_id_fkey"
+FOREIGN KEY (user_id) REFERENCES users(id)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+NOT DEFERRABLE;
+
+ 
+
+
+ 
--- hasura-ecomm/migrations/default/1654852829540_messages/up.sql
+++ hasura-ecomm/migrations/default/1654852829540_messages/up.sql
@@ -1,15 +1,16 @@
 CREATE TABLE "public"."messages" (
-
   "id" TEXT,
-
   "user_id" TEXT NOT NULL,
-
   "room_id" TEXT NOT NULL,
-
   "message" TEXT NOT NULL,
-
   "created_at" TIMESTAMPTZ DEFAULT NOW() NOT NULL,
-
   CONSTRAINT "messages_pkey" PRIMARY KEY ("id")
-
 );
+
+
+ALTER TABLE ONLY "public"."messages"
+ADD CONSTRAINT "messages_room_id_fkey"
+FOREIGN KEY (room_id) REFERENCES rooms(id)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+NOT DEFERRABLE;
