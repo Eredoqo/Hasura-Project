95718427628604c1206a9043a55d116f2e23e3fe
Eredoqo <eredoqorri96@gmail.com>
2022-06-10 09:54:18 +0000

    git push Create table View

--- hasura-ecomm/migrations/default/1654852829530_rooms/up.sql
+++ hasura-ecomm/migrations/default/1654852829530_rooms/up.sql
@@ -1,15 +1,13 @@
 
 CREATE TABLE "public"."rooms" (
+  "id" TEXT,
+  "user_id" TEXT NOT NULL,
+  "name" TEXT ,
+  "created_at" TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+  CONSTRAINT "rooms_pkey" PRIMARY KEY ("id")
+);
 
-"id" TEXT,
 
-"user_id" TEXT NOT NULL,
-
-"created_at" TIMESTAMPTZ DEFAULT NOW() NOT NULL,
-
-CONSTRAINT "rooms_pkey" PRIMARY KEY ("id")
-
- );
 ALTER TABLE ONLY "public"."rooms"
 ADD CONSTRAINT "rooms_user_id_fkey"
 FOREIGN KEY (user_id) REFERENCES users(id)
